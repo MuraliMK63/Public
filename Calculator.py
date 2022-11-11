c599e976375bb90bac143bb658ee2da6d731f946
MuraliMK63 <117427754+MuraliMK63@users.noreply.github.com>
2022-11-11 09:48:23 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Calculator Program at one line
+# Here I used eval function to Evaluate the String Expression into Python Expression and returns the result of the expression
+# This Code Snippet process only one expression
+print(eval(input("Enter Your Expression:")))
+
+# If you need to process multiple expression you can use the below code
+
+'''while True:
+    print(eval(input("Enter Your Expression:")))'''
